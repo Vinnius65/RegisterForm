3bfcacfc8a4db26697c0d264eb0737f0f2980760
Vinnius65 <125452036+Vinnius65@users.noreply.github.com>
2023-11-24 15:28:34 +0300

    register commit

---
+++ src/Register.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+public class Register {
+
+    public Register() {
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
